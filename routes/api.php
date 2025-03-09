5d7dc9d5117a04a46f29872cd585a5e88cf587e7
ashdiqqi <diqqifatriya@gmail.com>
2025-03-09 21:10:47 +0700

    api fitur kegiatan

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KegiatanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route test API
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API is working properly',
+        'timestamp' => now()
+    ]);
+});
+
+Route::prefix('kegiatan')->group(function () {
+    Route::get('/', [KegiatanController::class, 'index']);              // Get semua kegiatan
+    Route::post('/', [KegiatanController::class, 'store']);             // Membuat kegiatan baru
+    Route::get('/{id}', [KegiatanController::class, 'show']);          // Get detail kegiatan
+    Route::put('/{id}', [KegiatanController::class, 'update']);        // Update kegiatan
+    Route::delete('/{id}', [KegiatanController::class, 'destroy']);    // Hapus kegiatan
+});
+
+
